f530794a7cc6408c120e6e58414a8d0b8cfdeafc
Daniel Everland <danieleverland@gmail.com>
2018-07-04 21:43:43 +0200

    Added a list of stack traces for events

--- Assets/SO Architecture/Editor/Inspectors/GameEventEditor.cs
+++ Assets/SO Architecture/Editor/Inspectors/GameEventEditor.cs
@@ -1,18 +1,180 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using UnityEditor;
+using UnityEditor.AnimatedValues;
 
 [CustomEditor(typeof(GameEvent))]
 public class GameEventEditor : Editor
 {
+    private GameEvent Target { get { return target as GameEvent; } }
+
+    private const float PADDING = 30;
+    private const float HEADER_HEIGHT = 18;
+    private const float BACKGROUND_HEIGHT = 250;
+    private const float LINE_HEIGHT = 20;
+    private const string STACK_TRACE_WINDOW_KEY = "StackTraceWindow";
+
+    private const float CLEAR_LEFT_PADDING = 3;
+    private const float CLEAR_WIDTH = 45;
+
+    private Rect _rect;
+    private Vector2 _scrollPosition;
+    private GameEventStackTrace _selectedTrace;
+
+    private void OnEnable()
+    {
+        Deselect();
+    }
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
-        GUI.enabled = Application.isPlaying;
+        DrawRaiseButton();
 
+        EditorGUILayout.Space();
+
+        DrawStackTrace();
+    }
+    private void DrawRaiseButton()
+    {
         if (GUILayout.Button("Raise"))
         {
-            (target as GameEvent).Raise();
+            Target.Raise();
         }
+    }
+    private void DrawStackTrace()
+    {
+        Rect rect = GUILayoutUtility.GetRect(GUIContent.none, Styles.Box);
+        rect.height = BACKGROUND_HEIGHT;
+        
+        if (Event.current.type == EventType.Repaint)
+        {
+            //This is necessary due to Unity's retarded handling of events - https://answers.unity.com/questions/515197/how-to-use-guilayoututilitygetrect-properly.html
+            _rect = rect;
+
+            Rect boxRect = _rect;
+
+            boxRect.x--;
+            boxRect.y--;
+
+            boxRect.width++;
+            boxRect.height++;
+
+            Styles.Box.Draw(boxRect, GUIContent.none, 0);
+        }            
+
+        GUILayout.BeginArea(_rect);
+
+        DrawStackTraceHeader();
+        DrawElements();
+
+        GUILayout.EndArea();
+    }
+    private void DrawElements()
+    {
+        Rect elementsRect = new Rect()
+        {
+            y = HEADER_HEIGHT,
+            height = _rect.height - HEADER_HEIGHT,
+            width = _rect.width,
+        };
+        
+        Rect scrollViewRect = new Rect()
+        {
+            y = elementsRect.y,
+            height = elementsRect.height,
+            width = elementsRect.width,
+        };
+        Rect position = new Rect()
+        {
+            height = Target.StackTraces.Count * LINE_HEIGHT,
+            width = scrollViewRect.width - 20,
+        };
+
+        _scrollPosition = GUI.BeginScrollView(scrollViewRect, _scrollPosition, position);
+
+        for (int i = 0; i < Target.StackTraces.Count; i++)
+        {
+            GameEventStackTrace currentTrace = Target.StackTraces[i];
+            string currentText = GetFirstLine(currentTrace);
+
+            Rect elementRect = new Rect()
+            {
+                width = elementsRect.width,
+                height = LINE_HEIGHT,
+                y = i * LINE_HEIGHT,
+            };
+
+            if(Event.current.type == EventType.MouseDown && elementRect.Contains(Event.current.mousePosition))
+            {
+                _selectedTrace = currentTrace;
+
+                Repaint();
+            }
+            else if (Event.current.type == EventType.Repaint)
+            {
+                bool isSelected = _selectedTrace == currentTrace;
+                GUIStyle backgroundStyle = i % 2 == 0 ? Styles.EvenBackground : Styles.OddBackground;
+                
+                backgroundStyle.Draw(elementRect, false, false, isSelected, false);
+                Styles.Text.Draw(elementRect, new GUIContent(currentText), 0);
+            }
+        }
+        
+        GUI.EndScrollView();
+    }
+    private void DrawStackTraceHeader()
+    {
+        Rect rect = new Rect()
+        {
+            width = _rect.width,
+            height = HEADER_HEIGHT,
+        };
+
+        if(Event.current.type == EventType.Repaint)
+        {
+            Styles.Header.Draw(rect, new GUIContent("Stack Trace"), 0);
+        }
+
+        rect.x += CLEAR_LEFT_PADDING;
+        rect.width = CLEAR_WIDTH;
+
+        if (GUI.Button(rect, new GUIContent("Clear"), Styles.HeaderButton))
+        {
+            Target.StackTraces.Clear();
+            Deselect();
+        }
+    }
+    private void Deselect()
+    {
+        _selectedTrace = null;
+    }
+    private string GetFirstLine(string value)
+    {
+        return value.Split(new[] { '\r', '\n' }).FirstOrDefault();
+    }
+
+    private static class Styles
+    {
+        static Styles()
+        {
+            Box = new GUIStyle("CN Box");            
+            EvenBackground = new GUIStyle("CN EntryBackEven");
+            OddBackground = new GUIStyle("CN EntryBackodd");
+            HeaderButton = new GUIStyle("ToolbarButton");
+
+            Header = new GUIStyle("Toolbar");
+            Header.alignment = TextAnchor.MiddleCenter;
+
+            Text = new GUIStyle("CN EntryInfo");
+            Text.padding = new RectOffset(0, 0, 3, 0);
+        }
+
+        public static GUIStyle HeaderButton;
+        public static GUIStyle Text;
+        public static GUIStyle Box;
+        public static GUIStyle Header;
+        public static GUIStyle EvenBackground;
+        public static GUIStyle OddBackground;
     }
 }
--- Assets/SO Architecture/Events/GameEvent.cs
+++ Assets/SO Architecture/Events/GameEvent.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using System;
+#endif
+
 [CreateAssetMenu(fileName = "GameEvent.asset", menuName = SOArchitecture_Utility.GAME_EVENT, order = SOArchitecture_Utility.ASSET_MENU_ORDER)]
 public class GameEvent : ScriptableObject
 {
+#if UNITY_EDITOR
+    public List<GameEventStackTrace> StackTraces = new List<GameEventStackTrace>();
+#endif
+
     private readonly List<GameEventListener> _listeners = new List<GameEventListener>();
 
     public void Raise()
     {
+#if UNITY_EDITOR
+        StackTraces.Insert(0, GameEventStackTrace.Create());
+#endif
+
         for (int i = _listeners.Count - 1; i >= 0; i--)
             _listeners[i].OnEventRaised();
     }
@@ -22,3 +34,64 @@ public class GameEvent : ScriptableObject
             _listeners.Remove(listener);
     }
 }
+#if UNITY_EDITOR
+public class GameEventStackTrace : IEquatable<GameEventStackTrace>
+{
+    private GameEventStackTrace() { }
+    private GameEventStackTrace(string trace)
+    {
+        _id = UnityEngine.Random.Range(int.MinValue, int.MaxValue);        
+        _stackTrace = trace;
+
+        if (Application.isPlaying)
+        {
+            _frameCount = Time.frameCount;
+        }
+    }
+
+    private readonly int _id;
+    private readonly int _frameCount;
+    private readonly string _stackTrace;
+
+    public static GameEventStackTrace Create()
+    {
+        return new GameEventStackTrace(Environment.StackTrace);
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj == null)
+            return false;
+        
+        if(obj is GameEventStackTrace)
+        {
+            return Equals(obj as GameEventStackTrace);
+        }
+
+        return false;
+    }
+    public bool Equals(GameEventStackTrace other)
+    {
+        return other._id == this._id;
+    }
+    public override int GetHashCode()
+    {
+        return _id;
+    }
+    public override string ToString()
+    {
+        if(_frameCount > 0)
+        {
+            return string.Format("{0} {1}", _frameCount, _stackTrace);
+        }
+        else
+        {
+            return _stackTrace;
+        }        
+    }    
+
+    public static implicit operator string(GameEventStackTrace trace)
+    {
+        return trace.ToString();
+    }
+}
+#endif
