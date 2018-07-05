4e0edf20733aa0046c957c922f09f4e2b1a2ec67
Daniel Everland <danieleverland@gmail.com>
2018-07-05 23:49:55 +0200

    Fixed variable advanced ordering

--- Assets/SO Architecture/SOArchitecture_Utility.cs
+++ Assets/SO Architecture/SOArchitecture_Utility.cs
@@ -7,6 +7,6 @@
     public const string GAME_EVENT = "Game Event";
     public const string OBJECT_SETS = SETS_SUBMENU + "Objects";
 
-    public const int ADVANCED_ASSET_MENU_ORDER = 140;
+    public const int ADVANCED_ASSET_MENU_ORDER = 129;
     public const string ADVANCED_VARIABLE_SUBMENU = VARIABLE_SUBMENU + "Advanced/";
 }
