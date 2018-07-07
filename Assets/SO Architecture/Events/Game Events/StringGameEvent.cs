7b341a403c610cb54c6a91e2a6c9ac7f634e96f6
Daniel Everland <danieleverland@gmail.com>
2018-07-07 22:35:34 +0200

    Moved string event out of advanced sub-menu

--- Assets/SO Architecture/Events/Game Events/StringGameEvent.cs
+++ Assets/SO Architecture/Events/Game Events/StringGameEvent.cs
@@ -3,8 +3,8 @@
 [System.Serializable]
 [CreateAssetMenu(
     fileName = "StringGameEvent.asset",
-    menuName = SOArchitecture_Utility.ADVANCED_GAME_EVENT + "String",
-    order = SOArchitecture_Utility.ADVANCED_ASSET_MENU_ORDER)]
+    menuName = SOArchitecture_Utility.GAME_EVENT + "String",
+    order = SOArchitecture_Utility.ASSET_MENU_ORDER)]
 public sealed class StringGameEvent : GameEvent
 {
     [SerializeField]
