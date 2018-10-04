ad7ed931bf9a8ec42b2bc4a0ac4a4b3129f6ad4d
Daniel Everland <danieleverland@gmail.com>
2018-10-04 15:24:26 +0200

    Added media

---
+++ Assets/SO Architecture/Editor/Inspectors/SOArchitectureBaseObjectEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SOArchitectureBaseObject), true)]
+public class SOArchitectureBaseObjectEditor : Editor {
+
+    private SOArchitectureBaseObject Target { get { return (SOArchitectureBaseObject)target; } }
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Target.DeveloperDescription = DrawDescription(Target.DeveloperDescription);
+    }
+    public static string DrawDescription(string text)
+    {
+        string toReturn = text;
+        
+        EditorGUILayout.LabelField(new GUIContent("Description", "Click below this field to add a description"));
+
+        EditorGUI.indentLevel++;
+            toReturn = EditorGUILayout.TextArea(toReturn, Styles.TextAreaStyle);
+        EditorGUI.indentLevel--;
+        
+        return toReturn;
+    }
+
+    private static class Styles
+    {
+        public static GUIStyle TextAreaStyle;
+
+        static Styles()
+        {
+            TextAreaStyle = new GUIStyle(EditorStyles.textArea);
+            TextAreaStyle.normal = EditorStyles.label.normal;
+        }
+    }
+}
