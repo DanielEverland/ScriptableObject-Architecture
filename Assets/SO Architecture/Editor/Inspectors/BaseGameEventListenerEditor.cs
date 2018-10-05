999a0043e925b8446ef9f752c9b309c3e5d8a429
Daniel Everland <danieleverland@gmail.com>
2018-10-05 15:31:58 +0200

    Redone base event listener UI

--- Assets/SO Architecture/Editor/Inspectors/BaseGameEventListenerEditor.cs
+++ Assets/SO Architecture/Editor/Inspectors/BaseGameEventListenerEditor.cs
@@ -9,18 +9,28 @@ public class BaseGameEventListenerEditor : Editor
     private SerializedProperty DeveloperDescrption { get { return serializedObject.FindProperty("DeveloperDescription"); } }
 
     private StackTrace _stackTrace;
+    private SerializedProperty _event;
+    private SerializedProperty _debugColor;
+    private SerializedProperty _response;
     
     protected virtual void OnEnable()
     {
-        _stackTrace = new StackTrace(Target);
+        _stackTrace = new StackTrace(Target, true);
         _stackTrace.OnRepaint.AddListener(Repaint);
+
+        _event = serializedObject.FindProperty("_event");
+        _debugColor = serializedObject.FindProperty("_debugColor");
+        _response = serializedObject.FindProperty("_response");
     }
     public override void OnInspectorGUI()
     {
-        //EditorGUILayout.Space();
+        EditorGUILayout.ObjectField(_event, new GUIContent("Event", "Event which will trigger the response"));
+        _debugColor.colorValue = EditorGUILayout.ColorField(new GUIContent("Debug Color", "Color used to draw debug gizmos in the scene"), _debugColor.colorValue);
+
+        EditorGUILayout.PropertyField(_response, new GUIContent("Response"));
 
         _stackTrace.Draw();
 
-        //SOArchitectureBaseObjectEditor.DrawDescription(DeveloperDescrption);
+        SOArchitectureBaseObjectEditor.DrawDescription(DeveloperDescrption);
     }
 }
