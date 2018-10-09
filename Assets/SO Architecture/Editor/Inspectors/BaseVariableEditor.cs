7dc3a4e2c72d11094f6f74c619157bec81f95768
Daniel Everland <danieleverland@gmail.com>
2018-10-09 17:37:03 +0200

    Added developer description to variables

---
+++ Assets/SO Architecture/Editor/Inspectors/BaseVariableEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BaseVariable<>), true)]
+public class BaseVariableEditor : Editor
+{
+    private SerializedProperty _valueProperty;
+    private SerializedProperty _developerDescription;
+
+    private void OnEnable()
+    {
+        _valueProperty = serializedObject.FindProperty("_value");
+        _developerDescription = serializedObject.FindProperty("DeveloperDescription");
+    }
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.PropertyField(_valueProperty);
+        EditorGUILayout.PropertyField(_developerDescription);
+    }
+}
