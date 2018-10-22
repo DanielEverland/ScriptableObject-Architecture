24794c7913d72a58580de7fa2e940deb866b942a
Daniel Everland <danieleverland@gmail.com>
2018-10-22 14:03:09 +0200

    Commented runtime set editor inspector code regarding handling of scene objects

--- Assets/SO Architecture/Editor/Inspectors/RuntimeSetEditor.cs
+++ Assets/SO Architecture/Editor/Inspectors/RuntimeSetEditor.cs
@@ -36,9 +36,21 @@ public class RuntimeSetEditor : Editor
 
         SerializedProperty property = _reorderableList.serializedProperty.GetArrayElementAtIndex(index);
         
+        //We differentiate between the two so we can show scene objects that are assigned during runtime
+        //Assets cannot contain scene objects, so we disallow assigning out of runtime
+        //Note that scene objects must be assigned through code. Due to the above statement, the Unity editor specifically disallows it
+
         if(Application.isPlaying)
         {
-            EditorGUI.ObjectField(rect, "Element " + index, property.objectReferenceValue, _targetType, true);
+            //Very important not to assign to property here. Due to the comments made above, doing so will make all scene object entries null
+            Object obj = EditorGUI.ObjectField(rect, "Element " + index, property.objectReferenceValue, _targetType, false);
+
+            //If the object is a scene object we do nothing. This allows scene objects assigned through code to be visible in the editor,
+            //    while still allowing designers to manually assign non-scene objects
+            if (EditorUtility.IsPersistent(obj))
+            {
+                property.objectReferenceValue = obj;
+            }
         }
         else
         {
