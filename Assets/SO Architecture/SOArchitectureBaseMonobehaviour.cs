aa93372ff7166dc32e0969e9940b6c1a85bf69f6
Daniel Everland <danieleverland@gmail.com>
2018-10-23 20:25:25 +0200

    Fixed some warnings

--- Assets/SO Architecture/SOArchitectureBaseMonobehaviour.cs
+++ Assets/SO Architecture/SOArchitectureBaseMonobehaviour.cs
@@ -6,8 +6,11 @@
 /// </summary>
 public abstract class SOArchitectureBaseMonobehaviour : MonoBehaviour
 {
+
 #if UNITY_EDITOR
+#pragma warning disable 0414
     [SerializeField]
     private DeveloperDescription DeveloperDescription = new DeveloperDescription();
+#pragma warning restore
 #endif
 }
--- Assets/SO Architecture/SOArchitectureBaseObject.cs
+++ Assets/SO Architecture/SOArchitectureBaseObject.cs
@@ -7,7 +7,9 @@
 public abstract class SOArchitectureBaseObject : ScriptableObject
 {
 #if UNITY_EDITOR
+#pragma warning disable 0414
     [SerializeField]
     private DeveloperDescription DeveloperDescription = new DeveloperDescription();
+#pragma warning restore
 #endif
 }
