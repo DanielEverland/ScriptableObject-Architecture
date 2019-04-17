f4eb0ae68b4e9d7903ef60e838c4f676cfad4c0c
Jeff Campbell <jeff@zapdot.com>
2019-04-17 08:55:15 +0200

    PR Test Content
    * This is content added to make it easier to test this PR in the editor, it can be discarded after reviewing

---
+++ Assets/PR Test Content/LoadSceneOnAwake.cs
@@ -0,0 +1,19 @@
+﻿using ScriptableObjectArchitecture;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PR_Test_Content
+{
+    public class LoadSceneOnAwake : MonoBehaviour
+    {
+        public SceneReference sceneReference;
+
+        private void Awake()
+        {
+            if (sceneReference.Value.IsSceneEnabled && sceneReference.Value.IsSceneInBuildSettings)
+            {
+                SceneManager.LoadScene(sceneReference.Value.SceneName, LoadSceneMode.Additive);
+            }
+        }
+    }
+}
---
+++ Assets/PR Test Content/SceneReferenceExample.cs
@@ -0,0 +1,20 @@
+﻿using ScriptableObjectArchitecture;
+using UnityEngine;
+
+namespace PR_Test_Content
+{
+    public class SceneReferenceExample : MonoBehaviour
+    {
+        [SerializeField]
+        private SceneReference _sceneReference;
+
+        [SerializeField]
+        private SceneReference[] _sceneReferences;
+
+        [SerializeField]
+        private IntReference _intReference;
+
+        [SerializeField]
+        private IntReference[] _intReferences;
+    }
+}
