ba6bf87eac6feeb92f53499eb6543507a5a3570c
Jeff Campbell <jeff@zapdot.com>
2019-04-17 08:43:08 +0200

    Modified base Variable and Reference Drawer
    * Modified DrawValue method of BaseVariableDrawer to be virtual.
    * Modified BaseReferenceDrawer to check if constant property height is greater than a single line, and if it is to display it inline below the field rather than to the right of the label. For existing value type reference types this will not change their UX, but for more complex types it allows for more space to be displayed across the entire width of the inspector. Popup will display as normal.

--- Assets/SO Architecture/Editor/Drawers/BaseReferenceDrawer.cs
+++ Assets/SO Architecture/Editor/Drawers/BaseReferenceDrawer.cs
@@ -13,21 +13,34 @@ namespace ScriptableObjectArchitecture.Editor
         {
         "Use Constant",
         "Use Variable"
-    };
+        };
 
-        /// <summary> Cached style to use to draw the popup button. </summary>
-        private GUIStyle popupStyle;
+        /// <summary>
+        /// Cached style to use to draw the popup button. Lazy loaded as GUIStyles cannot be initialized as
+        /// an inline static field.
+        /// </summary>
+        private static GUIStyle PopupStyle
+        {
+            get
+            {
+                if (_popupStyle == null)
+                {
+                    _popupStyle = new GUIStyle(GUI.skin.GetStyle("PaneOptions"))
+                    {
+                        imagePosition = ImagePosition.ImageOnly
+                    };
+                }
+
+                return _popupStyle;
+            }
+        }
+
+        private static GUIStyle _popupStyle;
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            if (popupStyle == null)
-            {
-                popupStyle = new GUIStyle(GUI.skin.GetStyle("PaneOptions"));
-                popupStyle.imagePosition = ImagePosition.ImageOnly;
-            }
-
             label = EditorGUI.BeginProperty(position, label, property);
-            position = EditorGUI.PrefixLabel(position, label);
+            var refValuePosition = EditorGUI.PrefixLabel(position, label);
 
             EditorGUI.BeginChangeCheck();
 
@@ -37,28 +50,44 @@ namespace ScriptableObjectArchitecture.Editor
             SerializedProperty variable = property.FindPropertyRelative("_variable");
 
             // Calculate rect for configuration button
-            Rect buttonRect = new Rect(position);
-            buttonRect.yMin += popupStyle.margin.top;
-            buttonRect.width = popupStyle.fixedWidth + popupStyle.margin.right;
-            position.xMin = buttonRect.xMax;
+            Rect buttonRect = new Rect(refValuePosition);
+            buttonRect.yMin += PopupStyle.margin.top;
+            buttonRect.yMax = buttonRect.yMin + EditorGUIUtility.singleLineHeight;
+            buttonRect.width = PopupStyle.fixedWidth + PopupStyle.margin.right;
+            refValuePosition.xMin = buttonRect.xMax;
 
-            // Store old indent level and set it to 0, the PrefixLabel takes care of it
-            int indent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
-
-            int result = EditorGUI.Popup(buttonRect, useConstant.boolValue ? 0 : 1, popupOptions, popupStyle);
+            int result = EditorGUI.Popup(buttonRect, useConstant.boolValue ? 0 : 1, popupOptions, PopupStyle);
 
             useConstant.boolValue = result == 0;
 
-            EditorGUI.PropertyField(position,
+            if (useConstant.boolValue && EditorGUI.GetPropertyHeight(constantValue) > EditorGUIUtility.singleLineHeight)
+            {
+                refValuePosition = EditorGUI.IndentedRect(new Rect
+                {
+                    position = new Vector2(position.x, position.y + EditorGUIUtility.singleLineHeight),
+                    size = new Vector2(position.width, EditorGUI.GetPropertyHeight(constantValue) +
+                                                       EditorGUIUtility.singleLineHeight)
+                });
+                GUI.Box(refValuePosition, string.Empty);
+            }
+
+            EditorGUI.PropertyField(refValuePosition,
                 useConstant.boolValue ? constantValue : variable,
                 GUIContent.none);
 
             if (EditorGUI.EndChangeCheck())
                 property.serializedObject.ApplyModifiedProperties();
 
-            EditorGUI.indentLevel = indent;
             EditorGUI.EndProperty();
         }
-    } 
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty useConstant = property.FindPropertyRelative("_useConstant");
+            return !useConstant.boolValue
+                ? EditorGUIUtility.singleLineHeight
+                : EditorGUIUtility.singleLineHeight * 2f +
+                  EditorGUI.GetPropertyHeight(property.FindPropertyRelative("_constantValue"));
+        }
+    }
 }
