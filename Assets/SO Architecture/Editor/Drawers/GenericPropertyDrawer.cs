9c4409728f3ed0d539ee1f059691386cf1da9fae
Jeff Campbell <jeff@zapdot.com>
2019-04-19 10:37:12 +0200

    Fixed issue with reorderable list elements
    * Renamed the existing method DrawPropertyDrawer to DrawPropertyDrawerLayout to indicate it used the native Unity auto-layout controls and created a new method using the older method name that uses the non-layout controls. Added method documentation to both explaining their usage.
    * Changed BaseVariableEditor to use the new DrawPropertyDrawerLayout method name.
    * Cleaned up CollectionEditor and removed several redundant calls to modify the rect, redundant callbacks, etc..
    * Fixed a bug in CollectionEditor where the ReorderableList elements were being drawn by EditorGUILayout methods. These elements act similarly to PropertyDrawers in that they do not respect the auto-layout system methods and need to drawn using the non-layout methods that accept a Rect. I have added usage of callback `elementHeightCallback` so that the height of the element rect can vary depending on the generic T type element's property drawer and changed the element drawing logic to use a non-layout version of the same method from GenericPropertyDrawer. The end result is that these elements are now drawn inline with the reorderable list element instead of below the entire list.

--- Assets/SO Architecture/Editor/Drawers/GenericPropertyDrawer.cs
+++ Assets/SO Architecture/Editor/Drawers/GenericPropertyDrawer.cs
@@ -5,7 +5,47 @@ namespace ScriptableObjectArchitecture.Editor
 {
     public static class GenericPropertyDrawer
     {
-        public static void DrawPropertyDrawer(System.Type type, SerializedProperty property, GUIContent errorLabel)
+        /// <summary>
+        /// Draws a property drawer using the <see cref="EditorGUI"/> methods and the area the drawer is drawn
+        /// in is determined by the passed <see cref="Rect"/> <paramref name="rect"/>.
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="type"></param>
+        /// <param name="property"></param>
+        /// <param name="errorLabel"></param>
+        public static void DrawPropertyDrawer(Rect rect, System.Type type, SerializedProperty property, GUIContent errorLabel)
+        {
+            if (SOArchitecture_EditorUtility.HasPropertyDrawer(type) || typeof(Object).IsAssignableFrom(type) || type.IsEnum)
+            {
+                //Unity doesn't like it when you have scene objects on assets,
+                //so we do some magic to display it anyway
+                if (typeof(Object).IsAssignableFrom(type)
+                    && !EditorUtility.IsPersistent(property.objectReferenceValue)
+                    && property.objectReferenceValue != null)
+                {
+                    using (new EditorGUI.DisabledGroupScope(true))
+                    {
+                        EditorGUI.ObjectField(rect, new GUIContent("Value"), property.objectReferenceValue, type, false);
+                    }
+                }
+                else
+                {
+                    EditorGUI.PropertyField(rect, property);
+                }
+            }
+            else
+            {
+                EditorGUI.LabelField(rect, errorLabel);
+            }
+        }
+
+        /// <summary>
+        /// Draws a property drawer using the <see cref="EditorGUILayout"/> methods.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="property"></param>
+        /// <param name="errorLabel"></param>
+        public static void DrawPropertyDrawerLayout(System.Type type, SerializedProperty property, GUIContent errorLabel)
         {
             if (SOArchitecture_EditorUtility.HasPropertyDrawer(type) || typeof(Object).IsAssignableFrom(type) || type.IsEnum)
             {
--- Assets/SO Architecture/Editor/Inspectors/CollectionEditor.cs
+++ Assets/SO Architecture/Editor/Inspectors/CollectionEditor.cs
@@ -8,57 +8,86 @@ namespace ScriptableObjectArchitecture.Editor
     public class CollectionEditor : UnityEditor.Editor
     {
         private BaseCollection Target { get { return (BaseCollection)target; } }
-        private SerializedProperty DeveloperDescription { get { return serializedObject.FindProperty("DeveloperDescription"); } }
+        private SerializedProperty DeveloperDescriptionProperty
+        {
+            get { return serializedObject.FindProperty(DESCRIPTION_PROPERTY_NAME); }
+        }
+        private SerializedProperty CollectionItemsProperty
+        {
+            get { return serializedObject.FindProperty(LIST_PROPERTY_NAME);}
+        }
 
         private ReorderableList _reorderableList;
 
+        // UI
         private const bool DISABLE_ELEMENTS = false;
         private const bool ELEMENT_DRAGGABLE = true;
         private const bool LIST_DISPLAY_HEADER = true;
         private const bool LIST_DISPLAY_ADD_BUTTON = true;
         private const bool LIST_DISPLAY_REMOVE_BUTTON = true;
 
+        private const float ELEMENT_BOTTOM_PADDING = 2.5f;
+
+        private GUIContent _titleGUIContent;
+        private GUIContent _noPropertyDrawerWarningGUIContent;
+
+        private const string TITLE_FORMAT = "List ({0})";
+        private const string NO_PROPERTY_WARNING_FORMAT = "No PropertyDrawer for type [{0}]";
+
+        // Property Names
+        private const string LIST_PROPERTY_NAME = "_list";
+        private const string DESCRIPTION_PROPERTY_NAME = "DeveloperDescription";
+
         private void OnEnable()
         {
-            SerializedProperty items = serializedObject.FindProperty("_list");
+            _titleGUIContent = new GUIContent(string.Format(TITLE_FORMAT, Target.Type));
+            _noPropertyDrawerWarningGUIContent = new GUIContent(string.Format(NO_PROPERTY_WARNING_FORMAT, Target.Type));
 
-            string title = "List (" + Target.Type + ")";
-
-            _reorderableList = new ReorderableList(serializedObject, items, ELEMENT_DRAGGABLE, LIST_DISPLAY_HEADER, LIST_DISPLAY_ADD_BUTTON, LIST_DISPLAY_REMOVE_BUTTON);
-            _reorderableList.drawHeaderCallback += (Rect rect) => { EditorGUI.LabelField(rect, title); };
-            _reorderableList.drawElementCallback += DrawElement;
-            _reorderableList.onRemoveCallback += Remove;
-            _reorderableList.onChangedCallback += (ReorderableList list) => { Repaint(); };
+            _reorderableList = new ReorderableList(
+                serializedObject,
+                CollectionItemsProperty,
+                ELEMENT_DRAGGABLE,
+                LIST_DISPLAY_HEADER,
+                LIST_DISPLAY_ADD_BUTTON,
+                LIST_DISPLAY_REMOVE_BUTTON)
+            {
+                drawHeaderCallback = DrawHeader,
+                drawElementCallback = DrawElement,
+                elementHeightCallback = GetElementHeight,
+                onRemoveCallback = Remove
+            };
         }
         public override void OnInspectorGUI()
         {
-            serializedObject.Update();
+            EditorGUI.BeginChangeCheck();
 
             _reorderableList.DoLayoutList();
 
-            EditorGUILayout.PropertyField(DeveloperDescription);
+            EditorGUILayout.PropertyField(DeveloperDescriptionProperty);
 
-            serializedObject.ApplyModifiedProperties();
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+        private void DrawHeader(Rect rect)
+        {
+            EditorGUI.LabelField(rect, _titleGUIContent);
         }
         private void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
         {
-            rect.height = EditorGUIUtility.singleLineHeight;
-            rect.y++;
-
-            SerializedProperty property = _reorderableList.serializedProperty.GetArrayElementAtIndex(index);
+            SerializedProperty property = CollectionItemsProperty.GetArrayElementAtIndex(index);
 
             EditorGUI.BeginDisabledGroup(DISABLE_ELEMENTS);
 
-            EditorGUI.LabelField(rect, "Element " + index);
+            GenericPropertyDrawer.DrawPropertyDrawer(rect, Target.Type, property, _noPropertyDrawerWarningGUIContent);
 
-            rect.width /= 2;
-            rect.x += rect.width;
-
-            string content = "No PropertyDrawer for " + Target.Type;
-            GenericPropertyDrawer.DrawPropertyDrawer(Target.Type, property, new GUIContent(content, content));
-
-            //EditorGUI.ObjectField(rect, "Element " + index, property.objectReferenceValue, Target.Type, false);
             EditorGUI.EndDisabledGroup();
+        }
+        private float GetElementHeight(int index)
+        {
+            var indexedItemProperty = CollectionItemsProperty.GetArrayElementAtIndex(index);
+            return EditorGUI.GetPropertyHeight(indexedItemProperty) + ELEMENT_BOTTOM_PADDING;
         }
         private void Remove(ReorderableList list)
         {
