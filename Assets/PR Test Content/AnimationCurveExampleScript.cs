18fa57b041cacf845c3c707691304650b0385aee
Jeff Campbell <jeff@zapdot.com>
2019-04-20 10:35:04 +0200

    PR Content
    * This is PR content to make it easier to review and can be discarded after review+pr approval.

---
+++ Assets/PR Test Content/AnimationCurveExampleScript.cs
@@ -0,0 +1,14 @@
+﻿using ScriptableObjectArchitecture;
+using UnityEngine;
+
+namespace PR_Test_Content
+{
+    public class AnimationCurveExampleScript : MonoBehaviour
+    {
+        [SerializeField]
+        private AnimationCurveReference _animationCurveReference;
+
+        [SerializeField]
+        private AnimationCurveReference[] _animationCurveReferences;
+    }
+}
