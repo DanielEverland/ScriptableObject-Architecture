bb5da18752584c7d6d902ad164350655f48e1a74
Daniel Everland <danieleverland@gmail.com>
2019-04-21 04:15:22 +0200

    Moved Animation Curve create menus into advanced submenu

--- Assets/SO Architecture/Collections/AnimationCurveCollection.cs
+++ Assets/SO Architecture/Collections/AnimationCurveCollection.cs
@@ -4,7 +4,7 @@ namespace ScriptableObjectArchitecture
 {
 	[CreateAssetMenu(
 	    fileName = "AnimationCurveCollection.asset",
-	    menuName = SOArchitecture_Utility.COLLECTION_SUBMENU + "AnimationCurve",
+	    menuName = SOArchitecture_Utility.ADVANCED_VARIABLE_COLLECTION + "AnimationCurve",
 	    order = 120)]
 	public class AnimationCurveCollection : Collection<AnimationCurve>
 	{
--- Assets/SO Architecture/Variables/AnimationCurveVariable.cs
+++ Assets/SO Architecture/Variables/AnimationCurveVariable.cs
@@ -4,7 +4,7 @@ namespace ScriptableObjectArchitecture
 {
 	[CreateAssetMenu(
 	    fileName = "AnimationCurveVariable.asset",
-	    menuName = SOArchitecture_Utility.VARIABLE_SUBMENU + "AnimationCurve",
+	    menuName = SOArchitecture_Utility.ADVANCED_VARIABLE_SUBMENU + "AnimationCurve",
 	    order = 120)]
 	public class AnimationCurveVariable : BaseVariable<AnimationCurve>
 	{
