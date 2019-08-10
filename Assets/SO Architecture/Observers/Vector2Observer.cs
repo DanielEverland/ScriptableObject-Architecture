9000d3fb06628c3a5ae78472b05c6841f97b9f04
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2019-08-10 19:14:59 -0300

    Update Vector2Observer.cs

--- Assets/SO Architecture/Observers/Vector2Observer.cs
+++ Assets/SO Architecture/Observers/Vector2Observer.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace ScriptableObjectArchitecture
 {
-	[AddComponentMenu(SOArchitecture_Utility.OBSERVER_SUBMENU + "")]
+	[AddComponentMenu(SOArchitecture_Utility.OBSERVER_SUBMENU + "Vector2 Observer")]
 	public sealed class Vector2Observer : BaseObserver<Vector2, Vector2Variable, Vector2UnityEvent>
 	{
 	}
