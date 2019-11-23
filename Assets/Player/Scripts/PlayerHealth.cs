a168644281b09fef944cd764e002b9ce94c6d371
Daniel Everland <danieleverland@gmail.com>
2019-11-23 22:06:07 +0100

    Creating a player

---
+++ Assets/Player/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using ScriptableObjectArchitecture;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private FloatReference currentHealth = null;
+    [SerializeField]
+    private FloatReference maxHealth = null;
+
+    private void Awake()
+    {
+        currentHealth.Value = maxHealth.Value;
+    }
+
+
+public void HealthChanged()
+{
+    if (currentHealth.Value < 0)
+        Destroy(gameObject);
+}
+}
