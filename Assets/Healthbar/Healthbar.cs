e38687da777e945e89a5420d9535b25545be6d11
Daniel Everland <danieleverland@gmail.com>
2019-11-23 22:26:49 +0100

    Added healthbar

---
+++ Assets/Healthbar/Healthbar.cs
@@ -0,0 +1,20 @@
+﻿using ScriptableObjectArchitecture;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField]
+    private Image fillImage = null;
+    [SerializeField]
+    private FloatReference currentHealth = null;
+    [SerializeField]
+    private FloatReference maxHealth = null;
+
+    private void Update()
+    {
+        fillImage.fillAmount = currentHealth.Value / maxHealth.Value;
+    }
+}
