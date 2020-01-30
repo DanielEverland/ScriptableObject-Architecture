8b9604dae10ca31afb22738513e92aef158a2a40
Daniel Everland <danieleverland@gmail.com>
2020-01-30 17:41:49 +0100

    Moved BaseVariableWithEvent to namespace

--- Assets/SO Architecture/Variables/BaseVariableWithEvent.cs
+++ Assets/SO Architecture/Variables/BaseVariableWithEvent.cs
@@ -2,34 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using ScriptableObjectArchitecture;
 
-public class BaseVariableWithEvent<T, TEvent> : BaseVariable<T> where TEvent : UnityEvent<T>
+namespace ScriptableObjectArchitecture
 {
-    [SerializeField]
-    private TEvent _event = default;
+    public class BaseVariableWithEvent<T, TEvent> : BaseVariable<T> where TEvent : UnityEvent<T>
+    {
+        [SerializeField]
+        private TEvent _event = default;
 
-    public override T SetValue(T value)
-    {
-        T oldValue = _value;
-        T newValue = base.SetValue(value);
+        public override T SetValue(T value)
+        {
+            T oldValue = _value;
+            T newValue = base.SetValue(value);
 
-        if (!newValue.Equals(oldValue))
-            _event.Invoke(newValue);            
+            if (!newValue.Equals(oldValue))
+                _event.Invoke(newValue);
 
-        return newValue;
-    }
-    public void AddListener(UnityAction<T> callback)
-    {
-        _event.AddListener(callback);
-    }
-    public void RemoveListener(UnityAction<T> callback)
-    {
-        _event.RemoveListener(callback);
-    }
-    public override void RemoveAll()
-    {
-        base.RemoveAll();
-        _event.RemoveAllListeners();
+            return newValue;
+        }
+        public void AddListener(UnityAction<T> callback)
+        {
+            _event.AddListener(callback);
+        }
+        public void RemoveListener(UnityAction<T> callback)
+        {
+            _event.RemoveListener(callback);
+        }
+        public override void RemoveAll()
+        {
+            base.RemoveAll();
+            _event.RemoveAllListeners();
+        }
     }
 }
