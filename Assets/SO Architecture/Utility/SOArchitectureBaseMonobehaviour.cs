193e484c2b3712f8aa3a0ad188a831e1cf35156f
Daniel Everland <danieleverland@gmail.com>
2020-02-22 19:29:41 +0100

    Removed another reference to developer descriptions in summary of base SO-A class

--- Assets/SO Architecture/Utility/SOArchitectureBaseMonobehaviour.cs
+++ Assets/SO Architecture/Utility/SOArchitectureBaseMonobehaviour.cs
@@ -4,7 +4,6 @@ namespace ScriptableObjectArchitecture
 {
     /// <summary>
     /// Base class for SOArchitecture assets
-    /// Implements developer descriptions
     /// </summary>
     public abstract class SOArchitectureBaseMonobehaviour : MonoBehaviour
     {
