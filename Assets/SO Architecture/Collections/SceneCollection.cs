cec33823b4661eb60044ba70e73fe21062d8ad46
Daniel Everland <danieleverland@gmail.com>
2020-04-12 17:11:19 +0200

    Fixed SceneCollection referencing SceneVariable instead of SceneInfo

--- Assets/SO Architecture/Collections/SceneCollection.cs
+++ Assets/SO Architecture/Collections/SceneCollection.cs
@@ -6,7 +6,7 @@ namespace ScriptableObjectArchitecture
         fileName = "SceneCollection.asset",
         menuName = SOArchitecture_Utility.ADVANCED_VARIABLE_COLLECTION + "Scene",
         order = 120)]
-    public class SceneCollection : Collection<SceneVariable>
+    public class SceneCollection : Collection<SceneInfo>
     {
 
     }
