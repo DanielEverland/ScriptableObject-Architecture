b8432d1c5a76cd8f348cd7a313b43f2da2f9a58e
Daniel Everland <danieleverland@gmail.com>
2020-04-19 19:08:05 +0200

    Removed all test files

--- Assets/SO Architecture/CODE_GENERATION/Events/Listeners/TestTypeGameEventListener.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace ScriptableObjectArchitecture
-{
-	[AddComponentMenu(SOArchitecture_Utility.EVENT_LISTENER_SUBMENU + "TestType")]
-	public sealed class TestTypeGameEventListener : BaseGameEventListener<TestType, TestTypeGameEvent, TestTypeUnityEvent>
-	{
-	}
-}
